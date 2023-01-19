722416ac28c6fa03ec7ec1ab70f7cd827f9c7b3c
mivasconcelos <mvasconcelos@memsql.com>
2023-01-19 17:13:36 +0000

    use workers

--- bin/start.js
+++ bin/start.js
@@ -3,47 +3,9 @@
 const axios = require("axios");
 var createWorkspace = require("./create-workspace")
 const { execSync } = require('child_process');
-
-if (process.argv.length < 3) {
-  console.error("Please enter the name of your new app");
-  process.exit(1); //an error occurred
-}
-if (process.argv.length < 4) {
-  console.error("Please enter your key. More info on https://docs.singlestore.com/managed-service/en/reference/management-api.html#authorization");
-  process.exit(1); //an error occurred
-}
-
-const appName = process.argv.slice(2, 3)[0];
-const key = process.argv.slice(3, 4)[0];
-
-console.log(`
-                       oo 
-             ooOOOOOo     oOo
-        ooOOOOOOOOOOOOOOo   oOOOo
-     oOOOOOOOOOOOOOo oOOOOo  oOOOOo
-   oOOOOOOOOOo            oOo  oOOOOo
-  oOOOOOOOOo                 o  oOOOOo
- oOOOOOOOO                       oOOOOOo
-oOOOOOOOO                        oOOOOOo
-oOOOOOOOO                        oOOOOOo 
-oOOOOOOOO                       oOOOOOOo
-oOOOOOOOOO                    oOOOOOOOOo
- oOOOOOOOOOo                 oOOOOOOOOo
-  oOOOOOOOOOOo            oOOOOOOOOOOo
-   oOOOOOOOOOOOOOOoo oOOOOOOOOOOOOOoo
-     oOOOOOOOOOOOOOOOOOOOOOOOOOOOo
-        ooOOOOOOOOOOOOOOOOOOOoo
-             ooooOOOOOOoooo
-
-starting *${appName}*: an awesome SingleStore app!
-`)
-
-// const {hostname, password} = await createWorkspace.create(key).then((resp)=>{
-//   console.log("resp")
-// });
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 
 async function setupConnection(hostname, password) {
-  console.log("setup connection for:", hostname, password)
   try {
     const response = await axios({
       method: "POST",
@@ -62,23 +24,96 @@ async function setupConnection(hostname, password) {
   }
 }
 
-(async () => {
-console.log("start creating the workspace.....")
-  const { endpoint, password } = await createWorkspace.create(appName, key);
+if (isMainThread) {
+  const threads = new Set();;
 
-  const response = await setupConnection(endpoint, password);
-  console.log("Workspace created", response);
-})();
+  if (process.argv.length < 3) {
+    console.error("Please enter the name of your new app");
+    process.exit(1);
+  }
 
-execSync(`git clone https://github.com/singlestore-labs/singlestore-app-boilerplate.git ${appName}`)
+  const appName = process.argv.slice(2, 3)[0];
 
-execSync(`npm install`, {
-  cwd: './test',
-  stdio: 'inherit'
-})
+  if (process.argv.length < 4) {
+    console.error("Please enter your key. More info on https://docs.singlestore.com/managed-service/en/reference/management-api.html#authorization");
+    process.exit(1);
+  }
 
-execSync(`npm run dev`, {
-  cwd: './test',
-  stdio: 'inherit'
-})
+  const key = process.argv.slice(3, 4)[0];
+
+  threads.add(new Worker(__filename, { workerData: { type: "workspace", appName, key } }));
+  threads.add(new Worker(__filename, { workerData: { type: "app", appName, key } }));
+
+  for (let worker of threads) {
+    worker.on('error', (err) => { throw err; });
+    worker.on('exit', () => {
+      threads.delete(worker);
+    })
+    worker.on('message', (msg) => {
+      console.log(msg);
+    });
+  }
+
+} else {
+  // This code is executed in the worker and not in the main thread.
+
+  if (workerData.type === "app") {
+    console.log(`
+                           oo 
+                 ooOOOOOo     oOo
+            ooOOOOOOOOOOOOOOo   oOOOo
+         oOOOOOOOOOOOOOo oOOOOo  oOOOOo
+       oOOOOOOOOOo            oOo  oOOOOo
+      oOOOOOOOOo                 o  oOOOOo
+     oOOOOOOOO                       oOOOOOo
+    oOOOOOOOO                        oOOOOOo
+    oOOOOOOOO                        oOOOOOo 
+    oOOOOOOOO                       oOOOOOOo
+    oOOOOOOOOO                    oOOOOOOOOo
+     oOOOOOOOOOo                 oOOOOOOOOo
+      oOOOOOOOOOOo            oOOOOOOOOOOo
+       oOOOOOOOOOOOOOOoo oOOOOOOOOOOOOOoo
+         oOOOOOOOOOOOOOOOOOOOOOOOOOOOo
+            ooOOOOOOOOOOOOOOOOOOOoo
+                 ooooOOOOOOoooo
+    
+    starting *${workerData.appName}*: an awesome app powered by SingleStoreDB!
+    `)
+    try {
+      execSync(`git clone https://github.com/singlestore-labs/singlestore-app-boilerplate.git ${workerData.appName}`)
+    } catch (error) {
+      console.error(error)
+    }
+    
+    try {
+      execSync(`npm install`, {
+        cwd: `./${workerData.appName}`,
+        stdio: 'inherit'
+      })
+    } catch (error) {
+      console.error(error)
+    }
+
+    try {
+      execSync(`npm run dev`, {
+        cwd: `./${workerData.appName}`,
+        stdio: 'inherit'
+      })
+    } catch (error) {
+      console.error(error)
+    }
+
+    parentPort.postMessage('Your app is now ready!');
+  }
+  else if (workerData.type === "workspace") {
+    (async () => {
+      console.log("start creating your workspace...")
+      const { endpoint, password } = await createWorkspace.create(workerData.appName, workerData.key);
+      await setupConnection(endpoint, password);
+      parentPort.postMessage('Your workspace is ready!');
+    })();
+
+  }
+}
+
 
